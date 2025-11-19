81bdcc585ed2adb4708c5adf9fa94aeee1243171
kalpadas599 <kalpadas599@gmail.com>
2025-11-19 17:13:41 +0530

    python acova

---
+++ test01.py
@@ -0,0 +1,19 @@
+""" class tester:
+    def __init__(self, id):
+        self.id = str(id)
+        id="224"
+ 
+temp = tester(12)
+print(temp.id) """
+
+""" class tester:
+    def __init__(self, id):
+        self.id = str(id)
+        id="224"
+ 
+temp = tester(12)
+print(temp.id) """
+
+x = [[0], [1]]
+print((' '.join(list(map(str, x))),))
+
